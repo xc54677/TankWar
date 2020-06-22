9c2dcf1d43e31af3d865bb87de85b16b49c3a1c5
Xiao <xiao@192.168.0.102>
2020-06-22 23:04:02 +0800

    Initial commit

---
+++ src/test/ImageTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ImageTest {
+    @Test
+    public void test(){
+
+        try {
+            BufferedImage image = ImageIO.read(new File("/Users/xiao/IdeaProjects/TankWar/src/images/bulletD.gif"));
+            assertNotNull(image);
+
+            BufferedImage image2 = ImageIO.read(ImageTest.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+            assertNotNull(image2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
